585de0a65144e8e9e1e7f8f62c8c0a653b62317e
fleurishana <39075863+fleurishana@users.noreply.github.com>
2018-05-08 10:45:58 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link href="wow.css" rel="stylesheet" type="text/css" />
+
+<svg viewBox="0 0 1800 600">
+  <symbol id="s-text">
+    <text text-anchor="middle"
+          x="50%"
+          y="35%"
+          class="webcoderskull"
+          >
+    -*-*-*-*-*-*-       
+    </text>
+    <text text-anchor="middle"
+          x="50%"
+          y="68%"
+          class="text--line"
+          >
+     VJ - AGUNG
+    </text>
+        <text text-anchor="middle"
+          x="50%"
+          y="90%"
+          class="webcoderskull"
+          >
+    -*-*-*-*-*-*-
+    </text>
+  </symbol>
+  
+  <g class="g-ants">
+    <use xlink:href="#s-text"
+      class="webcoderskull-1"></use>     
+    <use xlink:href="#s-text"
+      class="webcoderskull-1"></use>     
+    <use xlink:href="#s-text"
+      class="webcoderskull-1"></use>     
+    <use xlink:href="#s-text"
+      class="webcoderskull-1"></use>     
+    <use xlink:href="#s-text"
+      class="webcoderskull-1"></use>     
+  </g>
+  
+  
+</svg>
